17e1a82df22951682e7780f5e053adf0dc93ac6b
carolinachm <carolti2013@gmail.com>
2018-10-03 05:35:38 -0300

    corrigindo a cliente service

--- src/main/java/back_vendas/service/ClienteService.java
+++ src/main/java/back_vendas/service/ClienteService.java
@@ -1,16 +1,14 @@
 package back_vendas.service;
 
-import back_vendas.model.Cliente;
-import back_vendas.repository.ClienteRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import back_vendas.model.Cliente;
+
 
 /**
  * Created by Leonardo Candido on 04/07/2018.
  */
-
+@Service
 public class ClienteService extends GenericService<Cliente> {
    
     
